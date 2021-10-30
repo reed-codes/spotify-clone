53b4104ab97633bf145115991243f30297831315
reed <helloreedemer@gmail.com>
2021-10-31 00:27:12 +0200

    resolved more errors causing build failure

--- pages/index.js
+++ pages/index.js
@@ -12,19 +12,19 @@ export default function Home() {
       <EditorsPickGrid/>
 
       <ContentSliderSection title = { getTitle( "top-tracks" ) } 
-                            url = { `/more/top-tracks?q=098${'&'}type=track` }
+                            url = { "/more/top-tracks?type=track&q=098" }
                               />
       <ContentSliderSection title = { getTitle( "top-albums" ) } 
-                            url = { `/more/top-albums?q=876${'&'}type=album` }
+                            url = { "/more/top-albums?type=album&q=876" }
                               />
       <ContentSliderSection title = { getTitle( "top-playlists" ) } 
-                            url = { `/more/top-playlists?q=654${'&'}type=playlist` }
+                            url = { "/more/top-playlists?type=playlist&q=654" }
                              />
       <ContentSliderSection title = { getTitle( "top-artists" ) } 
-                            url = { `/more/top-artists?q=321${'&'}type=artist` }
+                            url = { "/more/top-artists?type=playlist&q=321" }
                               />
       <ContentSliderSection title = { getTitle( `new-releases` ) } 
-                            url = { `/more/new-releases?q=632${'&'}type=track` }
+                            url = { "/more/new-releases?type=playlist&q=632" }
                              />
     </Container>
   );
